b305c30c1d3ceba275aeb8902447c80550f93bb4
Haikotjo <haikowierda@hotmail.com>
2023-08-03 03:29:32 +0200

    Button op profilepage zat nog een klein foutje in. En even een boodschap toegevoegd aan footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,7 +3,14 @@ import styles from "./Footer.module.scss"
 
 function Footer() {
     return (
-        <footer className={styles["footer"]}>© 2023 Haiko Wierda</footer>
+        <footer className={styles["footer"]}>
+            <div className={styles["footer-left"]}>
+                Made possible by Hogeschool Novi, with thanks to teacher Elwyn de Neve and SME'er Sam Barnhoorn. Thanks guys!
+            </div>
+            <div className={styles["footer-right"]}>
+                © 2023 Haiko Wierda
+            </div>
+        </footer>
     );
 }
 
